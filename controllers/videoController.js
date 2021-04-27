d30a6751216aa2474d47a4d591918bfe8c8636ff
Jiyeon-L <dldmsdud0721@naver.com>
2021-04-27 16:11:48 +0900

    else HTML

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,4 +1,7 @@
-export const home = (req, res) => res.render("home", {pageTitle : "Home"})
+
+export const home = (req, res) => {
+  res.render("home", { pageTitle: "Home", videos });
+};
 
 export const search = (req, res) => {
     const {
--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -4,9 +4,10 @@ import { deleteVideo, editVideo, upload, videoDetail, videos } from "../controll
 
 const videoRouter = express.Router();
 
-videoRouter.get(routes.upload, upload);
-videoRouter.get(routes.videoDetail, videoDetail);
 videoRouter.get(routes.editVideo, editVideo);
 videoRouter.get(routes.deleteVideo, deleteVideo);
+videoRouter.get(routes.upload, upload);
+videoRouter.get(routes.videoDetail, videoDetail);
+
 
 export default videoRouter;
