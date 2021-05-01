532db35539639acee058e381d6dfa7b66b4c347a
Jiyeon-L <dldmsdud0721@naver.com>
2021-05-01 14:53:58 +0900

    deleting a video

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,21 +1,30 @@
 import express from "express";
 import routes from "../routes";
 import { 
-    deleteVideo, 
-    editVideo,  
+    deleteVideo,   
     videoDetail, 
     getUpload,
-    postUpload, 
+    postUpload,
+    postEditVideo,
+    getEditVideo, 
 } from "../controllers/videoController";
 import { uploadVideo } from "../middlewares";
 
 const videoRouter = express.Router();
 
+//업로드
 videoRouter.get(routes.upload, getUpload);
 videoRouter.post(routes.upload, uploadVideo, postUpload);
+
+// 비디오 정보
 videoRouter.get(routes.videoDetail(), videoDetail);
-videoRouter.get(routes.editVideo, editVideo);
-videoRouter.get(routes.deleteVideo, deleteVideo);
+
+// 비디오 수정
+videoRouter.get(routes.editVideo(), getEditVideo);
+videoRouter.post(routes.editVideo(), postEditVideo);
+
+//비디오 삭제
+videoRouter.get(routes.deleteVideo(), deleteVideo);
 
 
 
